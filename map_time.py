2e43fddc2ec91a5181593525bbc9756268adfc81
amiteshsah <aks629@nyu.edu>
2017-04-04 17:12:24 -0400

    added the output format

--- map_time.py
+++ map_time.py
@@ -31,7 +31,34 @@ for line in sys.stdin:
 	Latitude=data[21]
 	Longitude=data[22]
 	Lat_Lon=data[23]
+
 	
-	
+	print '%s\t%s' % ((CMPLNT_NUM), \
+		str(CMPLNT_FR_DT)+str(', ') \
+		+str(CMPLNT_FR_TM)+str(', ') \
+		+str(CMPLNT_TO_DT)+str(', ') \
+		+str(CMPLNT_TO_TM)+str(', ') \
+		+str(CMPLNT_TO_DT)+str(', ') \
+		+str(CMPLNT_TO_TM)+str(', ') \
+		+str(RPT_DT)+str(', ') \
+		+str(KY_CD)+str(', ') \
+		+str(OFNS_DESC)+str(', ') \
+		+str(PD_CD)+str(', ') \
+		+str(PD_DESC)+str(', ') \
+		+str(CRM_ATPT_CPTD_CD)+str(', ') \
+		+str(LAW_CAT_CD)+str(', ') \
+		+str(JURIS_DESC)+str(', ') \
+		+str(BORO_NM)+str(', ') \
+		+str(ADDR_PCT_CD)+str(', ') \
+		+str(LOC_OF_OCCUR_DESC)+str(', ') \
+		+str(PREM_TYP_DESC)+str(', ') \
+		+str(PARKS_NM)+str(', ') \
+		+str(HADEVELOPT)+str(', ') \
+		+str(X_COORD_CD)+str(', ') \
+		+str(Y_COORD_CD)+str(', ') \
+		+str(Latitude)+str(', ') \
+		+str(Longitude)+str(', ') \
+		+str(Lat_Lon))
+
 
 
