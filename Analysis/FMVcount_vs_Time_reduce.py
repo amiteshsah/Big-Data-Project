0d729e0d5977e168db40b4613da5c3156d9e9700
amiteshsah <aks629@nyu.edu>
2017-04-12 16:53:31 -0400

    FMV vs time. Added FMVcount_vs_Time_map.py and FMVcount_vs_Time_reduce.py

---
+++ Analysis/FMVcount_vs_Time_reduce.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# map function to find the each type of LAW_CAT_CD commited in the different time of the day
+
+
+import sys
+import os
+
+currentkey=None
+current_count=0
+
+for line in sys.stdin:
+	line=line.strip()
+	key, count=line.split('\t',1)
+
+	try:
+		count = int(count)
+	except ValueError:
+		continue
+
+	if key == currentkey:
+		current_count+=count
+
+	else:
+		if currentkey:
+			print '%s\t%s' %(currentkey,current_count)
+
+		currentkey=key
+		current_count=count
+
+if key == currentkey:
+	print '%s\t%s' %(currentkey,current_count)
