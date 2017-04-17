6b7e61a382a382d1d3eefcf8dff58b01c98bd6ec
amiteshsah <aks629@nyu.edu>
2017-04-16 23:24:38 -0400

    map Count of valid , invalid and null for each column

--- CMPLNT_TO_TM.py
+++ CMPLNT_TO_TM.py
@@ -1,105 +1,66 @@
 #!/usr/bin/env python
 # reduce function for time
 
+# Column No 5: CMPLNT_TO_TM
 
 import sys
 import time
 import re
 import ast
+import csv
+
+valid_CMPLNT_TO_TM=0
+invalid_CMPLNT_TO_TM=0
+null_CMPLNT_TO_TM=0
 
 
+def CMPLNT_TO_TM_check(CMPLNT_TO_TM_1):
+	global valid_CMPLNT_TO_TM
+	global invalid_CMPLNT_TO_TM
+	global null_CMPLNT_TO_TM
 
-valid_CMPLNT_FR_TM=0
-invalid_CMPLNT_FR_TM=0
-null_CMPLNT_FR_TM=0
+	curr_time=CMPLNT_TO_TM_1
+	if(curr_time == ""):
+		return curr_time+"\t"+"DATETIME Complaint from time, NULL"
 
-invalid_X_COORD_CD=0
-valid_X_COORD_CD=0
-null_X_COORD_CD=0
+	curr_time_bool= re.search("([0-9]{2}.[0-9]{2}.[0-9]{2})", curr_time)
+	if curr_time_bool:
+		curr_time=curr_time[0]+curr_time[1]+":"+curr_time[3]+curr_time[4] +":" +curr_time[6]+curr_time[7]
+	else:
+		return curr_time+"\t"+"DATETIME Complaint from time, INVALID"
 
-valid_Y_COORD_CD=0
-invalid_Y_COORD_CD=0
-null_Y_COORD_CD=0
-
-
-def CMPLNT_FR_TM_check(CMPLNT_FR_TM_1):
-	global valid_CMPLNT_FR_TM
-	global invalid_CMPLNT_FR_TM
-	global null_CMPLNT_FR_TM
-
-	curr_time=CMPLNT_FR_TM_1[4]
-	if re.match('\d{2}:\d{2}:\d{2}', curr_time):
+	if re.match('\d{2}.\d{2}.\d{2}', curr_time):
 		try:
 			if time.strptime(curr_time, '%H:%M:%S'):
-				valid_CMPLNT_FR_TM+=1
+				valid_CMPLNT_TO_TM+=1
 				#return valid_CMPLNT_FR_TM
-				return 'DATETIME, time, VALID'
+				return curr_time+"\t"+"DATETIME Complaint from time, VALID"
 		except ValueError:
-			invalid_CMPLNT_FR_TM+=1
+			invalid_CMPLNT_TO_TM+=1
 			#return invalid_CMPLNT_FR_TM
-			return 'DATETIME, time, INVALID'
+			return curr_time+"\t"+"DATETIME Complaint from time, INVALID"
 	else:
-		null_CMPLNT_FR_TM+=1
+		null_CMPLNT_TO_TM+=1
 		#return null_CMPLNT_FR_TM
-		return 'DATETIME, time, NULL'
+		return curr_time+"\t"+"DATETIME Complaint from time, NULL"
 
 
-def X_COORD_CD_check(X_COORD_CD_20):
-	global invalid_X_COORD_CD
-	global valid_X_COORD_CD
-	global null_X_COORD_CD
+# for line in sys.stdin:
+# 	line=line.strip()
+# 	key, value=line.split('\t',1)
 
-	x_coord=X_COORD_CD_20
-	try:
-		x_coord=float(x_coord)
-		if x_coord<=1816290.753 and x_coord>127167.718:
-			valid_X_COORD_CD+=1
-			#return valid_X_COORD_CD
-			return 'COORDINATE, State plane coordinate , VALID'
-		else:
-			invalid_X_COORD_CD+=1
-			#return invalid_X_COORD_CD
-			return 'COORDINATE, State plane coordinate , INVALID'
-	except:
-		null_X_COORD_CD+=1
-		#return null_X_COORD_CD
-		return 'COORDINATE, State plane coordinate , NULL'
+# 	CMPLNT_TO_TM_1=ast.literal_eval(value)[3].strip()
+# 	CMPLNT_TO_TM=CMPLNT_FR_TM_check(CMPLNT_TO_TM_1)
+# 	print CMPLNT_TO_TM
 
 
-def Y_COORD_CD_check(Y_COORD_CD_21):
-	global valid_Y_COORD_CD
-	global invalid_Y_COORD_CD
-	global null_Y_COORD_CD
-	y_coord=Y_COORD_CD_21
-	try:
-		y_coord=float(y_coord)
-		if y_coord<=1598787.575 and y_coord>=-509684.437:
-			valid_Y_COORD_CD+=1
-			#return valid_Y_COORD_CD
-			return 'COORDINATE, State plane coordinate , VALID'
-		else:
-			invalid_Y_COORD_CD+=1
-			#return invalid_Y_COORD_CD
-			return 'COORDINATE, State plane coordinate , INVALID'
-	except:
-		null_Y_COORD_CD+=1
-		#return invalid_Y_COORD_CD
-		return 'COORDINATE, State plane coordinate , Null'
-
-
-
+firstline = True
 for line in sys.stdin:
-	line=line.strip()
-	key, value=line.split('\t',1)
+	data=list(csv.reader([line],delimiter=','))[0]
+	if firstline:    #skip first line
+		firstline = False
+		continue
+	CMPLNT_TO_TM=data[5].strip()
+	print(CMPLNT_TO_TM_check(CMPLNT_TO_TM))
 
-	CMPLNT_FR_TM_1=ast.literal_eval(value)[3].strip()
-	CMPLNT_FR_TM=CMPLNT_FR_TM_check(CMPLNT_FR_TM_1)
-
-	#X_COORD_CD_20=ast.literal_eval(value)[20].strip()
-	#X_COORD_CD=X_COORD_CD_check(X_COORD_CD_20)
-
-	#Y_COORD_CD_21=ast.literal_eval(value)[21].strip()
-	#Y_COORD_CD=Y_COORD_CD_check(Y_COORD_CD_21)
-
-	print CMPLNT_FR_TM_1+"	"+CMPLNT_FR_TM
 
--- Lat_Lon.py
+++ Lat_Lon.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append('.')
 from shapely import geometry
 import json
 from shapely.geometry import shape, Point
-import sys
 import ast
+import csv
 
 try:
 	with open ('./query.txt', 'r') as f:
@@ -10,28 +13,85 @@ try:
 except Exception as e:
 	print(e)
 
+
+valid_Lat_Lon=0
+invalid_Lat_Lon=0
+null_Lat_Lon=0
+
+
 def Lat_Lon_validator(line):
-	key,value = line.split("\t")
-	line=ast.literal_eval(value)
+
+	global invalid_Lat_Lon
+	global valid_Lat_Lon
+	global null_Lat_Lon
+
 	try:
 		# consp = truct point based on lat/long returned by geocoder
-		Lat_Lon_24=ast.literal_eval(line[24])
-		point=Point(float(Lat_Lon_24[1]),float(Lat_Lon_24[0]))
-		# check each polygon to see if it contains the point
-		flag=0
-		for feature in js['features']: 
-			polygon = shape(feature['geometry'])
-			if polygon.contains(point):
-				flag=1
-				break
-		if flag==1:
-			pass
-		else:
-			line[24]="INVALID"
+		Lat_Lon=ast.literal_eval(line)
 	except:
-		line[24]="NULL"
-	return line
-			
+		null_Lat_Lon+=1
+		return str(line)+"\t"+"COORDINATE Latitude and Longitude, NULL"
+		
+	point=Point(float(Lat_Lon[1]),float(Lat_Lon[0]))
+	# check each polygon to see if it contains the point
+	flag=0
+	for feature in js['features']: 
+		polygon = shape(feature['geometry'])
+		if polygon.contains(point):
+			flag=1
+			break
+	if flag==1:
+		valid_Lat_Lon+=1
+		return str(Lat_Lon)+"\t"+"COORDINATE Latitude and Longitude, VALID"
+	else:
+		invalid_Lat_Lon+=1
+		return str(Lat_Lon)+"\t"+"COORDINATE Latitude and Longitude, INVALID"
+
+
+# def Lat_Lon_validator(line):
+
+# 	global invalid_Lat_Lon
+# 	global valid_Lat_Lon
+# 	global null_Lat_Lon
+# 	objectValue = ast.literal_eval(line)
+
+# 	try:
+# 		# consp = truct point based on lat/long returned by geocoder
+# 		Lat_Lon=ast.literal_eval(objectValue[22])
+# 	except:
+# 		null_Lat_Lon+=1
+# 		return (objectValue[22],"COORDINATE", "Latitude and Longitude" , "NULL")
+		
+# 	point=Point(float(Lat_Lon[1]),float(Lat_Lon[0]))
+# 	# check each polygon to see if it contains the point
+# 	flag=0
+# 	for feature in js['features']: 
+# 		polygon = shape(feature['geometry'])
+# 		if polygon.contains(point):
+# 			flag=1
+# 			break
+# 	if flag==1:
+# 		valid_Lat_Lon+=1
+# 		return (Lat_Lon,"COORDINATE", "Latitude and Longitude" , "VALID")
+# 	else:
+# 		invalid_Lat_Lon+=1
+# 		return (Lat_Lon,"COORDINATE", "Latitude and Longitude" , "INVALID")
+
+
+# for line in sys.stdin:
+#     line=line.strip()
+#     key, value=line.split('\t',1)
+#     Lat_Lon=Lat_Lon_validator(value)
+#     print Lat_Lon
+
+
+firstline = True
 for line in sys.stdin:
-	temp_valriable = Lat_Lon_validator(line)
-	print temp_valriable
+    data=list(csv.reader([line],delimiter=','))[0]
+    if firstline:    #skip first line
+        firstline = False
+        continue
+    Lat_Lon=data[24].strip()
+    print(Lat_Lon_validator(Lat_Lon))
+
+
